0ad3e347820eac214dbe000d81f5d9d5390eef4a
zxjack <zxjack@gmail.com>
2023-04-06 17:10:30 +0800

    add pages

---
+++ build_index.py
@@ -0,0 +1,13 @@
+from bs4 import BeautifulSoup
+
+# Load all HTML files in the repository
+html_files = []
+for file_path in Path('.').rglob('*.html'):
+    with open(file_path, 'r') as f:
+        html_files.append(f.read())
+
+# Merge HTML files into a single index.html file
+merged_html = '\n'.join(html_files)
+soup = BeautifulSoup(merged_html, 'html.parser')
+with open('index.html', 'w') as f:
+    f.write(str(soup))
