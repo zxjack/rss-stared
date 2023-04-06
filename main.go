8f47a24275532852436c3e2c2c45088752ca0b25
zxjack <zxjack@gmail.com>
2023-04-06 13:55:13 +0800

    renew

--- main.go
+++ main.go
@@ -5,54 +5,55 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
-	"net/http"
 	"os"
-	"time"
 
 	"github.com/google/go-github/v38/github"
 	"golang.org/x/oauth2"
 )
 
 func main() {
-	// Authenticate with GitHub using a personal access token
 	token := os.Getenv("GITHUB_TOKEN")
 	if token == "" {
-		log.Fatal("GITHUB_TOKEN not set")
+		log.Fatal("GITHUB_TOKEN not found in environment")
 	}
-	ts := oauth2.StaticTokenSource(&oauth2.Token{AccessToken: token})
-	tc := oauth2.NewClient(context.Background(), ts)
+
+	ctx := context.Background()
+	ts := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: token},
+	)
+	tc := oauth2.NewClient(ctx, ts)
+
 	client := github.NewClient(tc)
 
-	// Fetch the contents of the RSS feed
-	feedURL := "https://r.ifyes.online:6443/public.php?op=rss&id=-1&is_cat=0&q=&key=xt9z0r6325e20d77277"
-	resp, err := http.Get(feedURL)
+	// 读取文件
+	file, err := os.Open("rss-stared/解决WIN11最让人不爽的2个问题-右键和任务栏.html")
 	if err != nil {
-		log.Fatal("Error fetching feed:", err)
+		log.Fatal(err)
 	}
-	defer resp.Body.Close()
+	defer file.Close()
 
-	body, err := ioutil.ReadAll(resp.Body)
+	content, err := ioutil.ReadAll(file)
 	if err != nil {
-		log.Fatal("Error reading response body:", err)
+		log.Fatal(err)
 	}
 
-	// Create a new commit with the RSS feed file
-	title := fmt.Sprintf("RSS feed %s", time.Now().Format("2006-01-02 15:04:05"))
-	message := "Automatically generated commit"
-	path := "rss-stared/rss.xml"
-	content := &github.RepositoryContentFileOptions{
-		Message: &message,
-		Content: body,
-	}
+	// 创建文件
+	repo := "zxjack/rss-stared"
+	owner := "zxjack"
 	branch := "main"
+	path := "rss-stared"
 
-	_, _, err = client.Repositories.CreateFile(context.Background(), "zxjack", "rss-stared", path, content, &github.RepositoryContentFileOptions{
-		Branch: &branch,
-	})
-
-	if err != nil {
-		log.Fatal("Error creating file in repository:", err)
+	opts := &github.RepositoryContentFileOptions{
+		Message: github.String("Add file"),
+		Content: content,
 	}
 
-	fmt.Println("Successfully created file in repository:", path)
+	_, _, err = client.Repositories.CreateFile(ctx, owner, repo, path, opts, &github.RepositoryContentFileOptions{
+		Branch: github.String(branch),
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("File added successfully")
 }
