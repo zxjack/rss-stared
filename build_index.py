db3842d17e97b4a46ed1d8966b1372469e762708
zxjack <zxjack@gmail.com>
2023-04-06 21:39:08 +0800

    修改build index

--- build_index.py
+++ build_index.py
@@ -1,6 +1,7 @@
 import os
 import requests
 from bs4 import BeautifulSoup
+from git import Repo
 
 # 获取需要的参数
 repo_name = os.getenv('GITHUB_REPOSITORY').split("/")[1]
@@ -8,6 +9,15 @@ branch_name = os.getenv('GITHUB_REF').split("/")[2]
 repo_url = f"https://github.com/{os.getenv('GITHUB_REPOSITORY')}"
 raw_files_url = f"{repo_url}/tree/{branch_name}"
 file_extension = ".html"
+publish_branch = "gh-pages"
+
+# 克隆要推送的分支并切换到其中
+if os.path.exists(publish_branch):
+    repo = Repo(publish_branch)
+    repo.git.checkout(publish_branch)
+else:
+    repo = Repo.clone_from(f"{repo_url}.git", publish_branch, branch=publish_branch)
+    repo.git.checkout(publish_branch)
 
 # 从GitHub仓库下载Raw文件列表
 response = requests.get(raw_files_url)
@@ -19,11 +29,17 @@ for a in soup.find_all('a', href=True):
     if a['href'].endswith(file_extension):
         html_files.append(a['href'])
 
-# 生成HTML链接列表
+# 构建HTML链接列表
 link_list = ""
 for f in html_files:
-    link_list += f'<a href="{repo_url}/blob/{branch_name}/{f}">{f}</a><br>'
+    file_name = os.path.basename(f)  # 获取文件名称
+    link_list += f'<a href="{f}">{file_name}</a><br>'
 
 # 创建index.html文件
 with open('index.html', 'w') as index_file:
     index_file.write(f'<html><body>{link_list}</body></html>')
+
+# 提交并推送到代码库
+repo.git.add("index.html")
+repo.git.commit("-m", "Update index.html")
+repo.git.push("origin", publish_branch)
